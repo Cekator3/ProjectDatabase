0a13ac0fd2e88533d9950e4607d529dac649b9e1
cekator3 <hamsterdreams@inbox.ru>
2024-02-04 23:54:21 +0500

    progress

--- 16_additional_services_of_ordered_model.sql
+++ 16_additional_services_of_ordered_model.sql
@@ -4,8 +4,11 @@ CREATE TABLE additional_services_of_ordered_model
 (
     ordered_model_id        integer NOT NULL,
     additional_service_id   integer NOT NULL,
+    -- Пользователь, добавивший доп услугу
+    user_id                 integer NOT NULL,
 
     UNIQUE (ordered_model_id, additional_service_id),
     FOREIGN KEY (ordered_model_id) REFERENCES ordered_models (id) ON DELETE CASCADE,
-    FOREIGN KEY (additional_service_id) REFERENCES additional_services (id) ON DELETE RESTRICT
+    FOREIGN KEY (additional_service_id) REFERENCES additional_services (id) ON DELETE RESTRICT,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
---
+++ 21_printer_distributing_models.sql
@@ -0,0 +1,11 @@
+-- распределяемые по принтерам модельки
+DROP TABLE IF EXISTS printer_distributing_models;
+CREATE TABLE printer_distributing_models
+(
+    user_id integer NOT NULL,
+    ordered_model_id integer NOT NULL,
+
+    UNIQUE (user_id, ordered_model_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (ordered_model_id) REFERENCES ordered_models (id) ON DELETE CASCADE
+);
