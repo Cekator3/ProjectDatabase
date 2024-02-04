dbba32b11f9ad7c7144e7df736d4ed7cac71ea3e
cekator3 <hamsterdreams@inbox.ru>
2024-02-04 11:41:38 +0500

    progress

---
+++ 19_calls_to_customer_history.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS calls_to_customer_history CASCADE;
+CREATE TABLE calls_to_customer_history
+(
+    id          integer     GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    order_id    integer     NOT NULL,
+    customer_id integer     NOT NULL,
+    called_at   timestamp   NOT NULL,
+    is_success  boolean     NOT NULL,
+
+    FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE,
+    FOREIGN KEY (customer_id) REFERENCES users (id) ON DELETE CASCADE
+);
